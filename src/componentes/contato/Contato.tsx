b60e22ffbbaa5a13e08e24385af9b842af71893e
EllynRP <evelyn.pereira2@estudante.ifms.edu.br>
2023-10-17 08:30:40 -0400

    frameowork171023

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,4 +1,8 @@
 import "./Contato.css"
+import peopleContato from '/peopleContato.png';
+import emailContato from '/emailContato.png';
+import phoneContato from '/phoneContato.png';
+
 
 function Contato(){
     return(
@@ -7,10 +11,26 @@ function Contato(){
             <h1 className="titleContato">Contate-me</h1>
             <div className="content-contato3">
               <div className="formularioDescricao">
-                <div>
-                  <p>imagem</p>
-                  <h3>titulo</h3>
-                  <p>"descrição"</p>
+                <div className="descricaoFormulario">
+                  <img src={peopleContato} alt="" />
+                  <div>
+                    <h3>Portofolio</h3>
+                    <p>Saiba mais sobre mim</p>
+                  </div>
+                </div>
+                <div className="descricaoFormulario">
+                  <img src={emailContato} alt="" />
+                  <div>
+                    <h3>Email</h3>
+                    <p>Evelyn.ramiro2005@gmail.com</p>
+                  </div>
+                </div>
+                <div  className="descricaoFormulario">
+                  <img src={phoneContato} alt="" />
+                  <div>
+                    <h3>Telefone</h3>
+                    <p>+55 67 90000-0000</p>
+                  </div>
                 </div>
               </div>
               <div className="formularioContato">
