f804e28f6d9ecdd781996c79dce30329cc5fce3f
EllynRP <evelyn.pereira2@estudante.ifms.edu.br>
2023-10-19 09:49:15 -0400

    trabalhop final de frameworks 191023

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,10 +1,31 @@
 import "./Contato.css"
+import {useState} from "react"
 import peopleContato from '/peopleContato.png';
 import emailContato from '/emailContato.png';
 import phoneContato from '/phoneContato.png';
 
 
 function Contato(){
+  const [nome, setNome] = useState("")  
+  const [email, setEmail] = useState("")
+  const [numero, setNumero] = useState("")
+  const [mensagem, setMensagem] = useState("")
+  function mudaNome(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.value)
+    setNome(e.target.value)
+  }
+  function mudaEmail(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.value)
+    setEmail(e.target.value)
+  }
+  function mudaNumero(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.value)
+    setNumero(e.target.value)
+  }
+  function mudaMensagem(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.value)
+    setMensagem(e.target.value)
+  }
     return(
         <div className='content-contato'>
           <div className="content-contato2">
@@ -36,20 +57,20 @@ function Contato(){
               <div className="formularioContato">
               <form>
                 <label>
-                  <p>Nome completo:</p>
-                  <input className="inputForm" type="text" name="Seu nome" />
+                  <p>Nome completo:{nome}</p>
+                  <input className="inputForm" type="text"  onChange={mudaNome}/>
                 </label>
                 <label>
-                  <p>Email:</p>
-                  <input className="inputForm" type="email" name="Seu email" />
+                  <p>Email:{email}</p>
+                  <input className="inputForm" type="email"  onChange={mudaEmail}/>
                 </label>
                 <label>
-                  <p>Numero de Telefone:</p>
-                  <input className="inputForm" type="text" name="seu numero de telefone" />
+                  <p>Numero de Telefone:{numero}</p>
+                  <input className="inputForm" type="text"  onChange={mudaNumero}/>
                 </label>
                 <label>
-                  <p>Mensagem:</p>
-                  <input className="inputForm" type="text" name="Sua mensagem" />
+                  <p>Mensagem:{mensagem}</p>
+                  <input className="inputForm" type="text"  onChange={mudaMensagem} />
                 </label>
                 <input className="botaoEnviar" type="submit" />
               </form>
@@ -59,6 +80,8 @@ function Contato(){
         </div>
 
     )
+
+    
 }
 
 export default Contato
